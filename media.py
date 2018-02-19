f566dc6114f2e06254c25483a684e7362ace90aa
GanaviJ <ganavijayaram1996@gmail.com>
2018-02-20 00:06:23 +0530

    python files according to pep8

--- media.py
+++ media.py
@@ -3,13 +3,15 @@ import webbrowser
 
 class Movie():
     # Assigning the variables
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title,
+                 movie_storyline,
+                 poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #To open the youtube trailer
+    # To open the youtube trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-    
