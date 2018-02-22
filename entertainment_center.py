64ca7ccfaaad287affd03a798e92a3231b639e1d
GanaviJ <ganavijayaram1996@gmail.com>
2018-02-22 22:21:04 +0530

    edited entertainment_center.py and media.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,17 +6,17 @@ import fresh_tomatoes
 toy_story = media.Movie("Toy story",
                         "A story of a boy and his toys which come to life",
                         "https://goo.gl/vQQPhg",
-                        "https://goo.gl/i5AvND")
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 captain_america = media.Movie("Captain America",
                               "A war amongst the hero's",
                               "https://goo.gl/zz4X93",
-                              "https://goo.gl/WoJVUk")
+                              "https://www.youtube.com/watch?v=SqLNDxbh1sM")
 
 justice_league = media.Movie("Justice League",
                              "Woman fights against her enemy",
                              "https://goo.gl/M8BLUD",
-                             "https://goo.gl/xYjL7s")
+                             "https://www.youtube.com/watch?v=r9-DM9uBtVI")
 
 # Creating a list to store the movies
 movies = [toy_story, captain_america, justice_league]
