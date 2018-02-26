5062a46f01b68776500ef2e7175a8c115990777e
GanaviJ <ganavijayaram1996@gmail.com>
2018-02-26 09:26:25 +0530

    chnaged the youtube trialers

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,17 +6,17 @@ import fresh_tomatoes
 toy_story = media.Movie("Toy story",
                         "A story of a boy and his toys which come to life",
                         "https://goo.gl/vQQPhg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc&t=5s")
 
 captain_america = media.Movie("Captain America",
                               "A war amongst the hero's",
                               "https://goo.gl/zz4X93",
-                              "https://www.youtube.com/watch?v=SqLNDxbh1sM")
+                              "https://www.youtube.com/watch?v=dKrVegVI0Us")
 
 justice_league = media.Movie("Justice League",
                              "Woman fights against her enemy",
                              "https://goo.gl/M8BLUD",
-                             "https://www.youtube.com/watch?v=r9-DM9uBtVI")
+                             "https://www.youtube.com/watch?v=3cxixDgHUYw")
 
 # Creating a list to store the movies
 movies = [toy_story, captain_america, justice_league]
